c89f4526bde113b1bc574ebaec5b6fad9d1b8883
muhrofi01 <75997807+muhrofi01@users.noreply.github.com>
2022-12-01 10:38:09 +0700

    Update useFetch.jsx

--- src/useFetch.jsx
+++ src/useFetch.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useMemo } from "react";
 // import tmdb from "./api/tmdb";
 import axios from "axios";
 
